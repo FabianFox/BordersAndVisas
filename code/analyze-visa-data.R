2e36a14391d238b4ae14a42f128e67c8612cbad5
FabianFox <fabianguelzau@web.de>
2021-09-21 15:50:18 +0200

    sent/received visas by regions

--- code/analyze-visa-data.R
+++ code/analyze-visa-data.R
@@ -66,6 +66,17 @@ visa.stats.df <- visa.stats.df %>%
                           custom_match = c("MKD" = "Europe & Central Asia",
                                            "DDR" = "Europe & Central Asia")))))
 
+# Variables grouped by region
+### ------------------------------------------------------------------------ ###
+visa.stats.df <- visa.stats.df %>%
+  mutate(data = map(data, ~.x %>%
+                      group_by(region) %>%
+                      mutate(region_mean_sent = mean(sent_waivers, na.rm = TRUE),
+                             region_mean_received = mean(received_waivers, na.rm = TRUE)) %>%
+                      ungroup() %>%
+                      mutate(global_mean_sent = mean(sent_waivers, na.rm = TRUE),
+                             global_mean_received = mean(received_waivers,na.rm = TRUE))))
+
 # Create variables for plotting
 ### ------------------------------------------------------------------------ ###
 visa.stats.df <- visa.stats.df %>%
@@ -83,31 +94,69 @@ visa.stats.df <- visa.stats.df %>%
            mutate(cum_sum = cumsum(n)/sum(n))
            ))
 
-# Plot
+# Plot histogram and cumulative frequency distribution across years
 ### ------------------------------------------------------------------------ ###
 visa.stats.df <- visa.stats.df %>%
-  mutate(barplot = 
-           map2(.x = plot_data, .y = year, ~ggplot(.x) +
-                 geom_bar(aes(x = interval, y = n), stat = "identity") +
-                 scale_y_continuous(limits = c(0, 40)) +
-                 scale_x_discrete(drop = FALSE) +
-                 labs(x = "", y = "", title = paste(.y)) +
-                 theme_minimal() +
-                 theme(
-                   axis.title.x = element_blank(),
-                   axis.text.x = element_text(angle = 45, hjust = 1)
-                 )),
-         lineplot = 
-           map2(.x = plot_data, .y = year, ~ggplot(.x) +
-                 geom_line(aes(x = interval, y = cum_sum, group = 1)) +
-                 geom_area(aes(x = interval, y = cum_sum, group = 1), 
-                           fill = "#252525", alpha = 0.4) +
-                 scale_x_discrete(drop = FALSE) +
-                 scale_y_continuous(labels = function(x) paste0(x * 100, "%")) +
-                 labs(x = "", y = "", title = paste(.y)) +
-                 theme_minimal() +
-                 theme(axis.text.x = element_text(angle = 45, hjust = 1))
-                 ))
+  mutate(
+    # Barplot of degree distribution
+    barplot =
+      map2(.x = plot_data, .y = year, ~ ggplot(.x) +
+        geom_bar(aes(x = interval, y = n), stat = "identity") +
+        scale_y_continuous(limits = c(0, 40)) +
+        scale_x_discrete(drop = FALSE) +
+        labs(x = "", y = "", title = paste(.y)) +
+        theme_minimal() +
+        theme(
+          axis.title.x = element_blank(),
+          axis.text.x = element_text(angle = 45, hjust = 1))),
+    # Cumulative frequency distribution of degree
+    lineplot =
+      map2(.x = plot_data, .y = year, ~ ggplot(.x) +
+        geom_line(aes(x = interval, y = cum_sum, group = 1)) +
+        geom_area(aes(x = interval, y = cum_sum, group = 1),
+          fill = "#252525", alpha = 0.4) +
+        scale_x_discrete(drop = FALSE) +
+        scale_y_continuous(labels = function(x) paste0(x * 100, "%")) +
+        labs(x = "", y = "", title = paste(.y)) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1))),
+    # Pointplot of received visas by regions
+    received_pointplot = map2(.x = data, .y = year, ~ ggplot(.x, aes(
+      x = factor(region, levels = rev(c(
+        "North America", "Europe & Central Asia", "Latin America & Caribbean",
+        "East Asia & Pacific", "Middle East & North Africa", "Sub-Saharan Africa",
+        "South Asia"))),
+      y = received_waivers)) +
+      geom_jitter(size = 2, alpha = 0.25, width = 0.2) +
+      geom_point(aes(x = region, y = region_mean_received),
+        size = 5, shape = 15, alpha = 0.05) +
+      geom_hline(aes(yintercept = global_mean_received),
+        color = "black",
+        size = 0.6, 
+        linetype = "dashed") +
+      coord_flip() +
+      ylim(0, 105) +
+      labs(x = "", y = "Receives visa waivers", title = paste(.y)) +
+      theme_minimal()),
+    # Pointplot of sent visas by regions
+    sent_pointplot = map2(.x = data, .y = year, ~ ggplot(.x, aes(
+      x = factor(region, levels = rev(c(
+        "North America", "Europe & Central Asia", "Latin America & Caribbean",
+        "East Asia & Pacific", "Middle East & North Africa", "Sub-Saharan Africa",
+        "South Asia"))),
+      y = sent_waivers)) +
+      geom_jitter(size = 2, alpha = 0.25, width = 0.2) +
+      geom_point(aes(x = region, y = region_mean_sent),
+        size = 5,
+        shape = 15, alpha = 0.05) +
+      geom_hline(aes(yintercept = global_mean_sent),
+        color = "black",
+        size = 0.6, linetype = "dashed") +
+      coord_flip() +
+      scale_y_continuous(limits = c(0,165), oob = scales::squish) +
+      labs(x = "", y = "Sent visa waivers", title = paste(.y)) +
+      theme_minimal())
+  )
 
 # Use patchwork to arrange plots
 ### ------------------------------------------------------------------------ ###
